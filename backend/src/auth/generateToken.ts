44267cbba69757d7db4976bede1d91f9ca91caee
Leonardo Montilla <leme.310589@gmail.com>
2022-10-24 19:01:31 -0400

    add function generateToken

---
+++ backend/src/auth/generateToken.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import { sign } from 'jsonwebtoken';
+import { UserModel } from '../interfaces/interfaces';
+
+const { ADMIN_TOKEN, USER_TOKEN } = process.env;
+
+export default async function generateToken(user: UserModel): Promise<string> {
+  try {
+    if (user.rol === 'admin') {
+      const token = await sign(user, (ADMIN_TOKEN as string));
+      return token;
+    }
+
+    const token = await sign(user, (USER_TOKEN as string));
+    return token;
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error al asignar un token', error);
+    return 'Error, no se pudo asignar un token';
+  }
+}
