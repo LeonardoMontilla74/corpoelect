fc252904ce79e85b84febf789e20d7fbe8e36fbb
Leonardo Montilla <leme.310589@gmail.com>
2022-10-25 15:18:50 -0400

    register usa un controlador

--- backend/src/routes/register.ts
+++ backend/src/routes/register.ts
@@ -1,9 +1,14 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
 import registerController from '../controllers/registerController';
+import { UserModel } from '../interfaces/interfaces';
 
 const router = Router();
 
-router.use('/', registerController);
+router.post('/', async (req: Request, res: Response) => {
+  const user: UserModel = req.body;
+  const result = await registerController(user);
+  res.status(201).json(result);
+});
 
 // eslint-disable-next-line import/prefer-default-export
 export { router };
