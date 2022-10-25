66a59643d8338f125fb00baa95b18051c3bbbd6b
Leonardo Montilla <leme.310589@gmail.com>
2022-10-24 21:37:47 -0400

    rutas automaticas

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,22 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
+import { readdirSync } from 'fs';
 
+const PATH = (__dirname);
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  res.status(200).json('Bienvenido a la API de corpoelect');
+function getNames(fileName: string) { // 'login.ts'
+  const names = fileName.split('.').shift(); // login
+  return names;
+}
+
+// eslint-disable-next-line array-callback-return
+readdirSync(PATH).map((file: string): void => {
+  const names = getNames(file);
+  if (names !== 'index') {
+    import(`./${names}`).then((object) => {
+      router.use(`/${names}`, object.router);
+    });
+  }
 });
 
 export default router;
