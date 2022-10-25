51898c09dc93117ef06002947b8724d6607987a8
Leonardo Montilla <leme.310589@gmail.com>
2022-10-25 14:24:14 -0400

    fix: ahora register no crea usuarios duplicados

--- backend/src/controllers/registerController.ts
+++ backend/src/controllers/registerController.ts
@@ -5,15 +5,19 @@ import Users from '../models/Users';
 
 const router = Router();
 
+// eslint-disable-next-line consistent-return
 router.post('/', async (req: Request, res: Response) => {
   const user: UserModel = req.body;
-  const passEncrypt = await hash((user.password as string), 8);
+
+  const checkExist = await Users.findOne({ where: { name: user.name } });
+  if (checkExist) return res.status(200).json(checkExist);
+
   try {
+    const passEncrypt = await hash((user.password as string), 8);
     const result = await Users.create({
       name: user.name,
       password: passEncrypt,
     });
-
     if (result) res.status(201).json(result);
   } catch (error) {
     // eslint-disable-next-line no-console
