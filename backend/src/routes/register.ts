cc5cd31cb30aa58895989fd0c3f12d92800d503c
Leonardo Montilla <leme.310589@gmail.com>
2022-10-26 09:09:34 -0400

    fix: manejo errores y continúa la app con next()

--- backend/src/routes/register.ts
+++ backend/src/routes/register.ts
@@ -1,13 +1,25 @@
-import { Request, Response, Router } from 'express';
+import {
+  NextFunction, Request, Response, Router,
+} from 'express';
 import registerController from '../controllers/registerController';
 import { UserModel } from '../interfaces/interfaces';
 
 const router = Router();
 
-router.post('/', async (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response, next: NextFunction) => {
   const user: UserModel = req.body;
-  const result = await registerController(user);
-  res.status(201).json(result);
+
+  try {
+    if (user.name && user.password) {
+      const result = await registerController(user);
+      res.status(201).json(result);
+    }
+    res.status(206).json({ msg: 'No suminitró los datos requeridos' });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Error antes de crear el usuario', error);
+    next();
+  }
 });
 
 // eslint-disable-next-line import/prefer-default-export
