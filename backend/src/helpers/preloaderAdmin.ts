8e865b9fc48f97e11992a1245758e9d4f9c60cc4
Leonardo Montilla <leme.310589@gmail.com>
2022-10-28 12:39:38 -0400

    check si ya existe el admin

--- backend/src/helpers/preloaderAdmin.ts
+++ backend/src/helpers/preloaderAdmin.ts
@@ -1,16 +1,23 @@
 import { hash } from 'bcrypt';
 import Users from '../models/Users';
+import handleError from './handleError';
 
 async function preloaderAdmin() {
-  const admin = await Users.create({
-    name: 'admin',
-    password: await hash('admin', 8),
-    rol: 'admin',
-    auth: true,
-  });
-  if (admin) {
-    // eslint-disable-next-line no-console
-    console.log('Administrador precargado');
+  const checkAdmin = await Users.findByPk(1);
+
+  try {
+    if (!checkAdmin) {
+      await Users.create({
+        name: 'admin',
+        password: await hash('admin', 8),
+        rol: 'admin',
+        auth: true,
+      });
+      // eslint-disable-next-line no-console
+      console.log('Administrador precargado');
+    }
+  } catch (error) {
+    handleError('Ya existe el administrador');
   }
 }
 
