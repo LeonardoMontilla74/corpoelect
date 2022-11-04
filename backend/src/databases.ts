cac9759e8aff89c27dc409bf85efd92a627a070a
Leonardo Montilla <leme.310589@gmail.com>
2022-11-03 23:12:44 -0400

    database

--- backend/src/databases.ts
+++ backend/src/databases.ts
@@ -5,31 +5,16 @@ const {
   DB_NAME, DB_USER, DB_PASS, DB_HOST,
 } = process.env;
 
-const sequelize = process.env.NODE_ENV === 'production'
-  ? new Sequelize({
-    database: DB_NAME,
+const sequelize = new Sequelize(
+  `${DB_NAME}`,
+  `${DB_USER}`,
+  `${DB_PASS}`,
+  {
+    host: `${DB_HOST}`,
     dialect: 'postgres',
-    host: DB_HOST,
-    port: 5432,
-    username: DB_USER,
-    password: DB_PASS,
-    pool: {
-      max: 3,
-      min: 1,
-      idle: 10000,
-    },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-      keepAlive: true,
-    },
-    ssl: true,
-  })
-  : new Sequelize(
-    `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`,
-    { logging: false, native: false },
-  );
+    logging: false,
+    native: false,
+  },
+);
 
 export default sequelize;
