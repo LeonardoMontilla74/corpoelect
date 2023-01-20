08984f20a470643dc6448d124aee26ffdbfac8bc
Leonardo Montilla <leme.310589@gmail.com>
2023-01-20 13:21:17 -0400

    add env

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'dotenv/config';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
@@ -9,7 +10,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import ClientsStoreProvider from './context/Clients/ClientsStoreProvider';
 import NotifStoreProvider from './context/Notifications/NotifStoreProvider';
 
-axios.defaults.baseURL = 'http://localhost:4000';
+const URL = process.env.REACT_APP_URL;
+
+axios.defaults.baseURL = URL || 'http://localhost:4000';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
