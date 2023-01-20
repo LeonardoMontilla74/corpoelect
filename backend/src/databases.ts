d2ea2d7ec129a3031a5b94536715bfb6a2fcc909
Leonardo Montilla <leme.310589@gmail.com>
2023-01-20 12:45:03 -0400

    conexión con postgres

--- backend/src/databases.ts
+++ backend/src/databases.ts
@@ -2,34 +2,36 @@ import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
 const {
-  DB_NAME, DB_USER, DB_PASS, DB_HOST,
+  DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT,
 } = process.env;
 
-const sequelize = process.env.NODE_ENV === 'production'
-  ? new Sequelize({
-    database: DB_NAME,
-    dialect: 'postgres',
-    host: DB_HOST,
-    port: 5432,
-    username: DB_USER,
-    password: DB_PASS,
-    pool: {
-      max: 3,
-      min: 1,
-      idle: 10000,
+const sequelize = new Sequelize(
+  `postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  { logging: false, native: false },
+);
+
+/*
+const sequelize = new Sequelize({
+  database: DB_NAME,
+  dialect: 'postgres',
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USER,
+  password: DB_PASS,
+  pool: {
+    max: 3,
+    min: 1,
+    idle: 10000,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-      keepAlive: true,
-    },
-    ssl: true,
-  })
-  : new Sequelize(
-    `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`,
-    { logging: false, native: false },
-  );
+    keepAlive: true,
+  },
+  ssl: true,
+});
+*/
 
 export default sequelize;
